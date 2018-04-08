2ce3005ca8c478308945ae4f3d38d1abdeee3be0
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-04-07 19:40:17 -0700

    works on large buckets, largely out of memory

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
+	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -147,6 +148,8 @@ func main() {
 		panic(err)
 	}
 
+	fmt.Fprintln(os.Stderr, "done getting keys, total ==", keyCount)
+
 	// fmt.Println("# of requests:", requests)
 	// fmt.Println("length of keys:", len(keys))
 
@@ -158,46 +161,10 @@ func main() {
 	var keynames map[string]int
 	keynames = make(map[string]int)
 
+	//
 	ch := make(chan kvp)
 
-	fmt.Fprintln(os.Stderr, "Got listing, getting tags....")
-
 	keysProcessed := 0
-
-	keyfile, err = os.Open(filepath.Join(tempDir, "keyfile.txt"))
-	scanner := bufio.NewScanner(keyfile)
-
-	// for _, key := range keys {
-	for scanner.Scan() {
-		key := scanner.Text()
-
-		keysProcessed++
-
-		if keysProcessed%1000 == 0 {
-			fmt.Fprintln(os.Stderr, "got ", keysProcessed, "of", keyCount)
-		}
-
-		guard <- struct{}{}
-		go getTags(bucketName, svc, key, ch, guard)
-		// tagInput := &s3.GetObjectTaggingInput{
-		// 	Bucket: aws.String(bucketName),
-		// 	Key:    key,
-		// }
-		// tagReq := svc.GetObjectTaggingRequest(tagInput)
-		// tagResult, err := tagReq.Send()
-		// if err != nil {
-		// 	panic(err)
-		// }
-		//
-		// for _, tag := range tagResult.TagSet {
-		// 	keynames[*tag.Key] = 1
-		// }
-		// keymap[*key] = tagResult.TagSet
-	}
-
-	fmt.Fprintln(os.Stderr, "number of keys is", keyCount)
-
-	keysProcessed = 0
 
 	jsonFile, err := os.Create(filepath.Join(tempDir, "records.json"))
 	if err != nil {
@@ -206,47 +173,84 @@ func main() {
 
 	_, err = jsonFile.WriteString("[\n")
 
-L:
-	for {
-		select {
-		case item := <-ch:
-			keysProcessed++
-			var bytesToWrite bytes.Buffer
+	var wg sync.WaitGroup
 
-			var strangs []string
-			strangs = append(strangs, fmt.Sprintf(`{ "key": "%s" `, item.key))
-			// bytesToWrite.WriteString(fmt.Sprintf(`{ "key": "%s", `, item.key))
-			// keymap[item.key] = item.tags
-			for _, tag := range item.tags {
-				keynames[*tag.Key] = 1
-				strangs = append(strangs, fmt.Sprintf(`"%s": "%s"`, *tag.Key, *tag.Value))
-			}
-			bytesToWrite.WriteString(strings.Join(strangs, ", "))
-			bytesToWrite.WriteString("}")
-			_, err = jsonFile.WriteString(bytesToWrite.String())
-			if err != nil {
-				panic(err)
-			}
-			// do stuf
-			if keysProcessed == keyCount {
-				break L
-			} else {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			// fmt.Fprintln(os.Stderr, "in select for loop and keysProcessed is", keysProcessed)
+
+			select {
+			case item := <-ch:
+				// fmt.Fprintln(os.Stderr, "keys processed:", keysProcessed)
+
+				keysProcessed++
+				var bytesToWrite bytes.Buffer
+
+				var strangs []string
+				strangs = append(strangs, fmt.Sprintf(`{ "key": "%s" `, item.key))
+				// bytesToWrite.WriteString(fmt.Sprintf(`{ "key": "%s", `, item.key))
+				// keymap[item.key] = item.tags
+				for _, tag := range item.tags {
+					keynames[*tag.Key] = 1
+					strangs = append(strangs, fmt.Sprintf(`"%s": "%s"`, *tag.Key, *tag.Value))
+				}
+				bytesToWrite.WriteString(strings.Join(strangs, ", "))
+				bytesToWrite.WriteString("}")
+				// fmt.Fprintln(os.Stderr, "writing to json file!")
+				_, err = jsonFile.WriteString(bytesToWrite.String())
+				if err != nil {
+					panic(err)
+				}
+				// do stuf
+				if keysProcessed == keyCount {
+
+					_, err = jsonFile.WriteString("\n]\n")
+					if err != nil {
+						panic(err)
+					}
+					err = jsonFile.Close()
+					if err != nil {
+						panic(err)
+					}
+
+					return
+				}
 				_, err = jsonFile.WriteString(",\n")
 				if err != nil {
 					panic(err)
 				}
 			}
 		}
-	}
 
-	_, err = jsonFile.WriteString("\n]\n")
-	if err != nil {
-		panic(err)
+	}()
+
+	fmt.Fprintln(os.Stderr, "Got listing, getting tags....")
+
+	keyfile, err = os.Open(filepath.Join(tempDir, "keyfile.txt"))
+	scanner := bufio.NewScanner(keyfile)
+
+	keysProcessed2 := 0
+
+	// for _, key := range keys {
+	for scanner.Scan() {
+		key := scanner.Text()
+
+		keysProcessed2++
+
+		if keysProcessed2%1000 == 0 {
+			fmt.Fprintln(os.Stderr, "got ", keysProcessed2, "of", keyCount)
+		}
+
+		guard <- struct{}{}
+		go getTags(bucketName, svc, key, ch, guard)
 	}
-	err = jsonFile.Close()
-	if err != nil {
-		panic(err)
-	}
+	// fmt.Fprintln(os.Stderr, "before wait")
+	wg.Wait()
+	// fmt.Fprintln(os.Stderr, "after wait")
+
+	fmt.Fprintln(os.Stderr, "number of keys is", keyCount)
 
 	// fmt.Println(keymap)
 	// fmt.Println("keynames:")
@@ -260,6 +264,7 @@ L:
 	fullTagColumns := append([]string{"key"}, tagColumns...)
 
 	w := csv.NewWriter(os.Stdout)
+	defer w.Flush()
 
 	if err2 := w.Write(fullTagColumns); err2 != nil {
 		panic(err2)
@@ -273,7 +278,11 @@ L:
 		panic(err)
 	}
 
+	count := 1
+
 	for dec.More() {
+		// fmt.Fprintln(os.Stderr, "writing record", count, "to csv")
+		count++
 		// for key := range keymap {
 		var m map[string]string
 		err = dec.Decode(&m)
